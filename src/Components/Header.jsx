18097dbd5103a945380b869e9238e271adc9509a
Jonathan Chosson <catstrike@gmail.com>
2023-01-24 15:23:54 +0100

    [JSX/CSS] Edition du menu de Nav :twisted_rightwards_arrows:

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,47 @@
 import React from "react"
 import "../Style/Components/Header.css"
+import { Link } from "react-router-dom"
+import Menu from "./Menu"
 
 const Header = () => {
     return (
         <div className="Header">
-            <h1 className="Header__h1">Au Rucher de Marches</h1>
+            <div className="Header__contain">
+                <img
+                    className="Header__img"
+                    src="./image/logo-rucher.png"
+                    alt="Au Rucher de Marches"
+                    title="Au Rucher de Marches"
+                />
+                <div className="Header__titre">
+                    <Link className="link" to={"/"}>
+                        <h1 className="Header__titre__h1">
+                            Au Rucher de Marches
+                        </h1>
+                    </Link>
+                    <img
+                        className="Header__titre__bio"
+                        src="./image/logo-bio.png"
+                        alt="Certifié bio"
+                        title="Certifié bio"
+                    />
+                </div>
+
+                <img
+                    className="Header__img"
+                    src="./image/logo-ferme.png"
+                    alt="La petite ferme de Maoliga"
+                    title="La petite ferme de Maoliga"
+                />
+                <div id="box">
+                    <img
+                        id="bee"
+                        src="https://www.freepnglogos.com/uploads/bee-png/bee-what-the-buzz-defining-health-buzz-words-cleanworkout-34.png"
+                        alt="Abeille"
+                    />
+                </div>
+            </div>
+            <Menu />
         </div>
     )
 }
