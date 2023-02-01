716d745d7df3ae5d10dc48c336ec1e42a9f2bb04
Jonathan Chosson <catstrike@gmail.com>
2023-02-01 11:43:12 +0100

    [JSX/CSS] Responsive menu navigation :iphone:

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -4,17 +4,32 @@ import ButtonNature from "./ButtonNature.jsx"
 
 const Menu = () => {
     return (
-        <nav className="Menu">
-            <ButtonNature text={"Accueil"} lien={"/"}></ButtonNature>
-            <ButtonNature text={"Histoire"} lien={"/"}></ButtonNature>
-            <ButtonNature text={"Nos produits"} lien={"/"}></ButtonNature>
-            <ButtonNature text={"Ferme pédagogique"} lien={"/"}></ButtonNature>
-            <ButtonNature text={"Prestations"} lien={"/"}></ButtonNature>
-            <ButtonNature text={"Galerie"} lien={"/"}></ButtonNature>
-            <ButtonNature text={"Blog"} lien={"/"}></ButtonNature>
-            <ButtonNature text={"Points de vente"} lien={"/"}></ButtonNature>
-            <ButtonNature text={"Contact/Horaires"} lien={"/"}></ButtonNature>
-        </nav>
+        <div className="Menu">
+            <i
+                className="fa-solid fa-bars Header__hamburger"
+                // onClick={toggleNav}
+            ></i>
+            <nav className="Menu__nav">
+                <ButtonNature text={"Accueil"} lien={"/"}></ButtonNature>
+                <ButtonNature text={"Histoire"} lien={"/"}></ButtonNature>
+                <ButtonNature text={"Nos produits"} lien={"/"}></ButtonNature>
+                <ButtonNature
+                    text={"Ferme pédagogique"}
+                    lien={"/"}
+                ></ButtonNature>
+                <ButtonNature text={"Prestations"} lien={"/"}></ButtonNature>
+                <ButtonNature text={"Galerie"} lien={"/"}></ButtonNature>
+                <ButtonNature text={"Blog"} lien={"/"}></ButtonNature>
+                <ButtonNature
+                    text={"Points de vente"}
+                    lien={"/"}
+                ></ButtonNature>
+                <ButtonNature
+                    text={"Contact/Horaires"}
+                    lien={"/"}
+                ></ButtonNature>
+            </nav>
+        </div>
     )
 }
 
