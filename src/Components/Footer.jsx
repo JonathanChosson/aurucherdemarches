e8539f612454e95633f7224baf1f76446f8866c6
Jonathan Chosson <catstrike@gmail.com>
2023-02-06 16:33:13 +0100

    [JSX/Style] Mise en place du footer :lipstick:

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import "../Style/Components/Footer.css"
+
+const Footer = () => {
+    return (
+        <div className="Footer">
+            <div className="Footer__social">
+                <i className="fa-brands fa-instagram Footer__social__insta"></i>
+                <p className="Footer__social__p">@aurucherdemarches</p>
+            </div>
+            <div className="Footer__social">
+                <i className="fa-brands fa-facebook Footer__social__fb"></i>
+                <p className="Footer__social__p">Au Rucher de Marches</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
