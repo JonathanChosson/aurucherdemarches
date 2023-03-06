3634ab3a94c577cc1c51791f80c2087dd3dc1056
Jonathan Chosson <catstrike@gmail.com>
2023-03-06 15:31:56 +0100

    [JSX/CSS] Finalisation squelette Histoire :rocket:

--- src/Pages/Histoire.jsx
+++ src/Pages/Histoire.jsx
@@ -7,11 +7,49 @@ const Histoire = () => {
     return (
         <div className="Histoire">
             <Header></Header>
-            <main className="Histoire__main border">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error
-                autem quaerat, sit fugit magnam dolore incidunt amet nesciunt
-                assumenda, at ipsum accusamus harum facilis optio quas
-                asperiores recusandae ex dignissimos?
+            <main className="Histoire__main">
+                <section className="Histoire__main__section">
+                    <img
+                        className="border Histoire__main__img"
+                        src="./image/Portrait-isa.jpg"
+                        alt="Protrait Isabelle Roux"
+                    />
+                    <div className="border Histoire__main__p">
+                        <h2>DATE</h2>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing
+                            elit. Quam dignissimos ipsa perspiciatis doloremque,
+                            obcaecati quis, eos itaque, eveniet dolor vero magni
+                            magnam repudiandae laudantium perferendis?
+                            Voluptatem fugiat perferendis quo dolore. Lorem
+                            ipsum dolor sit amet consectetur adipisicing elit.
+                            Asperiores et qui repellendus eum quod, hic fuga at
+                            vitae magni reprehenderit vero aut ipsa quos
+                            expedita aliquam libero, ut temporibus animi?
+                        </p>
+                    </div>
+                </section>
+                <section className="Histoire__main__section">
+                    <img
+                        className="border Histoire__main__img"
+                        src="./image/Portrait-isa.jpg"
+                        alt="Protrait Isabelle Roux"
+                    />
+                    <div className="border Histoire__main__p">
+                        <h2>DATE</h2>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing
+                            elit. Quam dignissimos ipsa perspiciatis doloremque,
+                            obcaecati quis, eos itaque, eveniet dolor vero magni
+                            magnam repudiandae laudantium perferendis?
+                            Voluptatem fugiat perferendis quo dolore. Lorem
+                            ipsum dolor sit amet consectetur adipisicing elit.
+                            Asperiores et qui repellendus eum quod, hic fuga at
+                            vitae magni reprehenderit vero aut ipsa quos
+                            expedita aliquam libero, ut temporibus animi?
+                        </p>
+                    </div>
+                </section>
             </main>
             <Footer></Footer>
         </div>
